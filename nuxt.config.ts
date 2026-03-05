423196f79181f2c7f3178e36cf8686512a069a60
alsongarbuja <magar33alson@gmail.com>
2026-03-05 16:30:06 -0600

    style: basic styling config with tailwindcss and css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/a11y', '@nuxt/image', '@nuxt/ui']
-})
+  css: ["./app/assets/css/main.css"],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  modules: ["@nuxt/a11y", "@nuxt/image", "@nuxt/ui"],
+});
