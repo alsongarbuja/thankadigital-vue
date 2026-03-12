bdf0342c1607ca1587025a0abcf527e2f3d7112f
alsongarbuja <magar33alson@gmail.com>
2026-03-12 01:59:39 -0500

    WIP: hero section design

---
+++ app/components/home/HeroSection.vue
@@ -0,0 +1,25 @@
+<template>
+  <section
+    class="relative min-h-[90vh] flex flex-col items-start justify-center px-8"
+  >
+    <div class="mx-auto z-10">
+      <h1 class="font-bold">Why Limit?</h1>
+
+      <div class="border-t border-black/10 pt-2">
+        <h3 class="text-sm uppercase tracking-widest text-gray-500">
+          When you can go beyond with us.
+        </h3>
+      </div>
+      <button class="group flex items-center gap-4">
+        <div
+          class="w-16 h-16 border border-black rounded-full flex items-center justify-center group-hover:bg-black group-hover:text-white transition-all duration-500"
+        >
+          <span class="text-xl">→</span>
+        </div>
+        <span class="uppercase tracking-widest text-xs font-bold">
+          Start Journey
+        </span>
+      </button>
+    </div>
+  </section>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-// import { projects } from "~/utils/datas/projects";
+import HeroSection from "~/components/home/HeroSection.vue";
 import ProjectsSection from "~/components/ProjectSection.vue";
 
 const { data: projects } = await useFetch<IProjectScheme[]>("/api/projects");
@@ -13,34 +13,7 @@ useSeoMeta({
 
 <template>
   <main>
-    <section class="relative min-h-[70vh] flex flex-col bg-[#fafafa] px-8">
-      <div
-        class="absolute inset-0 opacity-[0.03] pointer-events-none grain-overlay"
-      ></div>
-
-      <div class="container mx-auto px-4 z-10">
-        <h1 class="">Why Limit?</h1>
-
-        <div class="border-t border-black/10 pt-2">
-          <h2 class="text-sm uppercase tracking-widest text-gray-500">
-            When you can go beyond with us.
-          </h2>
-        </div>
-      </div>
-
-      <button
-        class="absolute bottom-20 right-10 md:right-32 group flex items-center gap-4"
-      >
-        <div
-          class="w-16 h-16 border border-black rounded-full flex items-center justify-center group-hover:bg-black group-hover:text-white transition-all duration-500"
-        >
-          <span class="text-xl">→</span>
-        </div>
-        <span class="uppercase tracking-widest text-xs font-bold">
-          Start Journey
-        </span>
-      </button>
-    </section>
+    <HeroSection />
 
     <ProjectsSection :projects="projects" />
   </main>
