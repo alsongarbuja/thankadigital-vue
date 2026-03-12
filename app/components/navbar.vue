c13e06f2795e29082124c4ccdfd55a5bf0ed8242
alsongarbuja <magar33alson@gmail.com>
2026-03-12 01:59:14 -0500

    util: add navbar link from utility

--- app/components/navbar.vue
+++ app/components/navbar.vue
@@ -1,18 +1,31 @@
+<script setup lang="ts">
+import { navigationLinks } from "~/utils/datas/header";
+</script>
+
 <template>
   <header class="py-4 px-8">
     <nav class="flex items-center justify-between">
-      <NuxtImg
-        src="icons/thanka_logo.svg"
-        alt="Thanka digital logo a blue and red infinite knot"
-        width="40"
-        height="40"
-      />
+      <NuxtLink href="/" aria-label="Thanka Digital Logo">
+        <NuxtImg
+          src="icons/thanka_logo.svg"
+          alt="Thanka digital logo a blue and red infinite knot"
+          width="40"
+          height="40"
+        />
+      </NuxtLink>
 
-      <div class="flex items-center gap-2">
-        <p>text 1</p>
-        <p>text 2</p>
-        <p>text 3</p>
-        <p>text 4</p>
+      <div class="flex items-center gap-8">
+        <NuxtLink
+          v-for="link in navigationLinks"
+          :key="link.id"
+          :href="link.to"
+          class="group w-fit"
+        >
+          {{ link.text }}
+          <hr
+            className="h-0.5 bg-black w-0 group-hover:w-full transition-[width] duration-300"
+          />
+        </NuxtLink>
       </div>
     </nav>
   </header>
---
+++ app/utils/datas/header.ts
@@ -0,0 +1,22 @@
+export const navigationLinks = [
+  {
+    id: 0,
+    text: "About",
+    to: "/about",
+  },
+  {
+    id: 1,
+    text: "Projects",
+    to: "/projects",
+  },
+  {
+    id: 0,
+    text: "Career",
+    to: "/careers",
+  },
+  {
+    id: 3,
+    text: "Blog",
+    to: "/blogs",
+  },
+];
