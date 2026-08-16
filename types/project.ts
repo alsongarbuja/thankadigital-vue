bef5895a52c586455bfdb709351efbaa3ab25148
alsongarbuja <magar33alson@gmail.com>
2026-08-16 12:35:00 -0500

    relocate: move types to root with some changes to exporting types

--- types/project.ts
+++ types/project.ts
@@ -1,16 +1,12 @@
-interface IProjectScheme {
+export type IProjectScheme = {
   name: string;
   slug: string;
   thumbnail: string;
   liveLink: string | null;
   githubLink: string | null;
-
   details: string;
   tags: string[];
-
   summary: string;
-
   status: "published" | "draft" | "archieved";
-
   isShowcase: boolean;
-}
+};
